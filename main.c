dd7c31e384752771780cf11e8b659e15c0e9ed1d
Gabe Corella <gabecorella@wifi-roaming-128-4-143-155.host.udel.edu>
2019-09-04 11:43:39 -0400

    Work on Main function..

--- main.c
+++ main.c
@@ -16,11 +16,44 @@ int main(){
 
     void insert(char *title, char *artist, int num, int time);
     void delete(char *name);
+    void log();
+    void removeList();
 
-    // Function Definitions
-    printf("---Welcome---");
-
+    char buffer[BUFFERSIZE];
+    char slct;
+    int len, i;
+    node_t *mp3;
+            // Print Function Definitions
+            printf("---Welcome---\n");
+            printf("_____________\n");
+            printf("Press a Number for Selection..\n");
+            printf("(1) Insert \n");
+            printf("(2) Delete \n");
+            printf("(3) Display \n");
+            printf("(4) Exit \n");
     head = NULL;
+// IF given a number < 0, then print an error message 
 
+        if(scanf("%d%c",&i,slct <0)){
+            printf("Error: Please")
+        }; 
+        switch (i)
+        {
+        case /* constant-expression */:
+            /* code */
+            break;
+        
+        default:
+            break;
+        }
+
+
+
+
+
+
+    // Free at the End
+    free(mp3->artist);
+    free(mp3);
     return 0;
 }
